ffce5dcaea8f0aa4dd8ecb935e97250310ef9aa6
Bora <b0rik556@gmail.com>
2022-07-07 14:38:40 +0300

    add styles and improve scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,81 +1,153 @@
-function add(number1, number2) {
-    return number1 + number2;
+function add() {
+    return parseFloat(first) + parseFloat(second);
 }
 
-function substract(number1, number2) {
-    return number1 - number2;
+function substract() {
+    return parseFloat(first) - parseFloat(second)
 }
 
-function multiply(number1, number2) {
-    return number1 * number2;
+function multiply() {
+    return parseFloat(first) * parseFloat(second);
 }
 
-function divide(number1, number2) {
-    if(number2 === 0) return;
-    return number1 / number2;
+function divide() {
+    if(parseFloat(second) === 0) return;
+    return parseFloat(first) / parseFloat(second);
 }
 
-function operate(operator, number1, number2) {
-    switch(operator) {
+
+function opResult() {
+    switch(op) {
         case '+':
-            return add(number1, number2);
+            return add().toString();
             break;
         case '-':
-            return substract(number1, number2);
+            return substract().toString();
             break;
         case '*':
-            return multiply(number1, number2);
+            return multiply().toString();
             break;
         case '/':
-            return divide(number1, number2);
+            let res = divide();
+            return res ? res.toString() : res;
             break;
         default:
-            return number2;
+            return second.toString();
             break;
     }
 }
 
 function updateDisplay() {
-    displayFirst.innerHTML = first;
-    displaySecond.innerHTML = second;
+    displayFirst.innerHTML = `${first} ${op}`;
+    displaySecond.innerHTML = second
 }
 
 function appendNumber(number) {
-    if(number === '.' && second.includes('.')) return;
+    if(number === '.' && (second === '' ||second.includes('.'))) return;
+    if(number === '0' && second === '0') return;
+
+    if(reset) {
+        clear();
+        reset = false;
+    }
+
+    if(isNewLine) {
+        first = second;
+        isNewLine = !isNewLine;
+        second = '';
+    }
 
     second = second + number;
+
+    updateDisplay();
+}
+
+function appendOperator(operator) {
+    if(reset) {
+        clear();
+        reset = false;
+    }
+
+    isNewLine = true;
+
+    if(second === '') {
+        isNewLine = false;
+    } else {
+        first = opResult()
+        if(first) {
+            second = first;
+        } else {
+            second = 'ERROR!';
+            reset = true;
+        }
+    }
+
+    if(first !== '') op = operator;
+
     updateDisplay();
 }
 
 function clear() {
     first = '';
     second = '';
+    result = 0;
     op = '';
+    isNewLine = false;
+
     updateDisplay();
 }
 
 function backspace() {
-    if(second == '') return;
+    if(reset) {
+        clear();
+        reset = false;
+    }
+
+    if(second === '') return;
 
     second = second.slice(0, -1);
+
+    if(second === '') isNewLine = false;
+
     updateDisplay();
 }
 
-function showResult() {
-    first = '';
-    second = operate().toString();
-}
-
 function equals() {
-    let firstNum = first === '' ? 0 : parseFloat(first);
-    let secondNum = parseFloat(second);
+    if(reset) {
+        clear();
+        reset = false;
+    }
 
-    let result = operate(op, firstNum, secondNum);
+    if(first === '' && second === '') { 
+        result = '';
+    } else if(first === '') {
+        result = second;
+    } else if(second === ''){
+        result = first;
+    } else {
+        result = opResult();
+    }
+
+    if(result) {
+        second = result;
+    } else {
+        second = 'ERROR!';
+        reset = true;
+    }
 
     first = '';
-    second = result == undefined ? 'ERROR' : result.toString();
+    op = '';
     
     updateDisplay()
+}
+
+function handleKey(key) {
+    if(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'].includes(key)) appendNumber(key);
+    if(['+', '-', ,'*', '/'].includes(key))  appendOperator(key);
+    if(['Enter', '='].includes(key)) equals();
+    if(key === 'c') clear();
+    if(key === 'Backspace') backspace();
+
 }
 
 const displayFirst = document.querySelector('.first');
@@ -89,9 +161,16 @@ const equalsButton = document.querySelector('.btn-equals');
 let first = '';
 let second = '';
 let op = '';
+let result = '';
+let isNewLine = false;
+let reset = false;
 
 numbers.forEach(button => {
     button.addEventListener('click', () => appendNumber(button.innerHTML));
+});
+
+operations.forEach(button => {
+    button.addEventListener('click', () => appendOperator(button.innerHTML));
 });
 
 clearButton.addEventListener('click', () => clear());
@@ -99,3 +178,5 @@ clearButton.addEventListener('click', () => clear());
 backButton.addEventListener('click', () => backspace());
 
 equalsButton.addEventListener('click', () => equals());
+
+document.addEventListener('keydown', e => handleKey(e.key));
